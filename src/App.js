9380903e0f2a2081ae59f9e007382580f3e429d9
kazomikun <106589367+kazomikun@users.noreply.github.com>
2024-03-03 16:55:47 +0000

    portfolio section
    added section and images, still need to add image overlays

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react"
 import NavBar from "./NavBar"
+import Masthead from "./Masthead"
+import PortfolioSection from "./PortfolioSection"
+import AboutSection from "./AboutSection"
+import ContactSection from "./ContactSection"
+import FooterSection from "./FooterSection"
 
 const App = () => {
   return (
+    <div>
     <NavBar />
+    <Masthead />
+    <PortfolioSection />
+    <AboutSection />
+    {/* <ContactSection />
+    <FooterSection /> */}
+    </div>
   )
 }
 
